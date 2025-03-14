bf832a2e59079207f0f458ef3b8e6eaffe5102ce
Dương Hải Lưu <162011769+DuongHaiLuu0904@users.noreply.github.com>
2025-03-15 00:45:19 +0700

    Thêm timyMCE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const flash = require('express-flash')
@@ -33,6 +34,8 @@ app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 // End Flash
 
+// TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
 app.use(express.static(`${__dirname}/public`))
 
